6c26fd3bb22490f02f26279b016869e7cdd138cf
1dlvb <matushkin-2017@mail.ru>
2021-03-03 23:10:52 +0400

    Add styles, contact & about pages.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
+@app.route("/home")
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
