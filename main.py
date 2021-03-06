3420e2bea65913d8f9bb4e14546224060f51fc7e
1dlvb <matushkin-2017@mail.ru>
2021-03-06 13:38:02 +0400

    Changed download.html template.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, g
 from downloader import YtDownloader, preview_image
 app = Flask(__name__)
 
@@ -7,15 +7,7 @@ yt = YtDownloader()
 
 @app.route("/home", methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        link = request.form["link"]
-        yt.initialisation(link)
-        title = yt.title()
-        preview_img = preview_image(yt.video_id())
-        print(title)
-        print(preview_img)
-
-    return render_template("index.html", link=link)
+    return render_template("index.html")
 
 
 @app.route("/about")
@@ -28,9 +20,18 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/download")
+@app.route("/download", methods=['POST', 'GET'])
 def download():
-    return render_template("download.html")
+    if request.method == 'POST':
+        link = request.form["link"]
+        yt.initialisation(link)
+        title = yt.title()
+        preview_img = preview_image(yt.video_id())
+
+        return render_template("download.html",  preview_img=preview_img, title=title, link=link)
+    else:
+        return "There was some kind of error"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
